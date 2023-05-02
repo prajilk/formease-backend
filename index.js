1c356abc990dcae66c3874f0762147872183b11f
Prajil K <prajilk20017@gmail.com>
2023-05-02 16:19:42 +0530

    Added url to cors

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://formease.vercel.app'],
+    origin: 'https://formease.vercel.app',
     credentials: true
 }))
 
